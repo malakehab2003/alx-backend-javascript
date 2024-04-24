96acb0b762d8393f64f3501068bbd8e317e0765b
root <lokaehab2003@gmail.com>
2024-04-24 00:43:16 +0000

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const idx of array) {
+    const value = appendString + idx;
+    newArray.push(value);
+  }
+
+  return newArray;
+}
