23c86241aa83ae8df20d59ef3c978b8bbcacf966
malakehab2003 <lokaehab2003@gmail.com>
2024-06-18 22:18:11 +0300

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+/**
+ * say hello and enter your name
+ */
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
+
+process.stdin.resume();
