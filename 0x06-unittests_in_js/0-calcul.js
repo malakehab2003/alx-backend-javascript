ab3c663eee59d5a8362c582655d47b7c7772e646
malakehab2003 <lokaehab2003@gmail.com>
2024-06-20 21:07:31 +0300

    task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,11 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+/**
+ * add two numbers
+ * @param {Float} a 
+ * @param {Float} b 
+ * @returns 
+ */
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
 
 module.exports = calculateNumber
