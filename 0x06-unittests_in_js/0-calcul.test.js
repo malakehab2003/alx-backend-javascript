873e7055539475a28eba31ffa46e99cffc1c01be
malakehab2003 <lokaehab2003@gmail.com>
2024-06-20 20:46:33 +0300

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,4 +13,20 @@ describe('test calculate', () => {
     it('test float number rounded to next number', () => {
         assert.strictEqual(calculateNumber(1.6, 2), 4)
     });
+
+    it('test numbers', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4)
+    });
+
+    it('test numbers', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5)
+    });
+
+    it('test numbers', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5)
+    });
+
+    it('test numbers', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+    });
 });
